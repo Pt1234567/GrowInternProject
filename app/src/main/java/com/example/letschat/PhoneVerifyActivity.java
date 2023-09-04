6c1dcfa14cf6c78b29b0413da1683f76328a491e
Pt1234567 <tripathiprashant45678@gmail.com>
2023-09-04 22:14:06 +0530

    First Commit

---
+++ app/src/main/java/com/example/letschat/PhoneVerifyActivity.java
@@ -0,0 +1,15 @@
+package com.example.letschat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneVerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_verify);
+
+    }
+}
